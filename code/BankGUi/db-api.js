c55ed1baaa91ac30893f260b2607f6720c75127f
BruddaCode <99719149+BruddaCode@users.noreply.github.com>
2025-04-17 15:49:47 +0200

    Update db-api.js

--- code/BankGUi/db-api.js
+++ code/BankGUi/db-api.js
@@ -1,8 +1,8 @@
-require('dotenv').config({path:'./conf.env'});
-const mysql = require('mysql2');
-const express = require('express');
-const app = express();
-const port = 9000;
+require('dotenv').config({path:'./conf.env'})
+const mysql = require('mysql2')
+const express = require('express')
+const app = express()
+const port = 9000
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -10,52 +10,103 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
-});
+})
 
 db.connect(err => {
     if (err) {
-        console.error('Connection failed:', err.stack);
-        return;
+        console.error('Connection failed:', err.stack)
+        return
     }
-    console.log('Connected as id', db.threadId);
-});
+    console.log('Connected as id', db.threadId)
+})
 
-app.use(express.json());
+app.use(express.json())
 
 app.get('/api/noob/health', (req, res) => {
-    res.json({status: "OK"});
-});
+    res.statusCode(200).json({status: "OK"})
+})
 
 // --- getinfo ---
 app.post('/api/noob/users/getinfo', (req, res) => {
-    try {
-        var iban = req.body.iban;
-        var pin = req.body.pin;
-    }
-    catch {
-        res.json({error: "bad request"}); // verander naar error code ipv json
-    }
+    // check if given json is correct
+    const responseData = checkJson(req.body, res)
+    if (!responseData) return
+    const {iban, pin} = responseData
 
-    // collom toevoegen om pas te blokeren
-
-    // check for sql-injection
-    // get data from db
-    // send back json
-    
-
-    
-    // res.json({data: req.body});
-});
+    db.query('SELECT * FROM rekeningen WHERE iban = ?', [iban], (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: err })
+        }
+        if (result[0] == null) {
+            return res.status(500).json({ error: "cant find user" })
+        }
+        if (result[0].actief == 0){
+            return res.status(409).json({ error: "card is blocked" })
+        }
+        if (pin != result[0].pin) {
+            return res.status(409).json({ error: "wrong pin" })
+        }
+        const {iban, saldo, valuta} = result[0]
+        res.json({iban, saldo, valuta})
+    })
+})
 
 // --- withdraw ---
-// app.put('/users/:id', (req, res) => {
-//     const { name, email } = req.body;
-//     db.query('UPDATE users SET name = ?, email = ? WHERE id = ?', [name, email, req.params.id], (err) => {
-//         if (err) return res.status(500).json({ error: err });
-//         res.json({ message: 'User updated' });
-//     });
-// });
+app.post('/api/noob/users/withdraw', (req, res) => {
+    const responseData = checkJson(req.body, res)
+    if (!responseData) return
+    const {iban, pin, amount} = responseData
+
+    db.query('SELECT * FROM rekeningen WHERE iban = ?', [iban], (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: err })
+        }
+        if (result[0] == null) {
+            return res.status(500).json({ error: "cant find user" })
+        }
+        if (result[0].actief == 0){
+            return res.status(409).json({ error: "card is blocked" })
+        }
+        if (pin != result[0].pin) {
+            return res.status(409).json({ error: "wrong pin" })
+        }
+        if (amount > result[0].saldo) {
+            return res.status(409).json({ error: "Insufficient Funds" })
+        }
+        var saldo = result[0].saldo - amount
+
+        db.query('UPDATE rekeningen SET saldo = ? WHERE iban = ?', [saldo, iban], (err) => {
+            if (err) {
+                return res.status(500).json({ error: err })
+            }
+            res.json({iban: iban, message: "Withdrawal successful"})
+        })
+    })
+})
 
 app.listen(port, () => {
-    console.log(`server op poort: ${port}`);
-});
+    console.log(`server op poort: ${port}`)
+})
+
+function checkJson(request, res) {
+    // check if keys are empty
+    for (const [key, value] of Object.entries(request)) {
+        if (!value) {
+            res.status(400).json({ error: `Missing or empty field: ${key}` })
+            return false 
+        }
+    }
+    
+    // check for sql-injection
+    for (const [key, value] of Object.entries(request)) {
+        if (Number.isInteger(value)) {
+            continue
+        }
+        if (value.includes("'")) {
+            res.status(500).json({error: "nice SQL"})
+            return false 
+        }
+    }
+
+    return request
+}
