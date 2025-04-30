ea7804b6eab61f3f22a4c209104f36e290297966
ActiveMoon <cornenrl05@gmail.com>
2025-04-30 14:54:37 +0200

    aanpassing om samenwerking makkelijker te integreren

--- code/bonnetje/bonnetje.ino
+++ code/bonnetje/bonnetje.ino
@@ -9,61 +9,24 @@ Adafruit_Thermal printer(&mySerial);
 
 void setup() {
   mySerial.begin(9600);
+  Serial.begin(9600);
   printer.begin();
 
   printer.setFont('B');
-  printer.setFont('A');
+  printer.setFont('A'); 
 
-  printer.feed(2);
-
-  left();
-  large();
-  printer.boldOn();
-  printer.println("SYMPLE");
-  printer.boldOff();
-
-  center();
-  printLine();
-
-  medium();
-  center();
-  printer.setLineHeight(10);
-  printer.println("OPNAME BILJETTEN");
-  printer.setLineHeight();
-
-  printer.feed(1);
-
-  left();
-  small();
-  printer.println("DATUM\t\tTIJD\t\tAUTOMAAT");
-  printer.println("01-01-2025\t12:00\t\t1");
-
-  printer.feed(1);
-
-  //Rekeningnummer ten alle tijden deels afgeschermd houden
-  printer.println("REKENINGNUMMER\t\t\tPASNR");
-  printer.println("NLxx SMPL xxxx xxxx xx\t1234");
-
-  printer.feed(1);
-
-  printer.println("TRANSACTIE ID\t\t:\t1");
-
-  printer.feed(1);
-
-  printer.println("BEDRAG\t\t\t\t:\t100,00");
-  printer.println("TRANSACTIE KOSTEN\t:\t0,00");
-
-  printer.feed(1);
-
-  printer.println("TOTAAL BEDRAG\t\t:\t100,00");
-
-  printer.feed(2);
-
-  medium();
-  center();
-  printer.println("TOT ZIENS");
-
-  printer.feed(4);
+  String datum = "01-01-2025";
+  String tijd = "12:00";
+  String iban = "44 12";
+  String pasnummer = "1234";
+  String transactieId = "1";
+  float b = 100.00;
+  float tk = 0.00;
+  float tb = b + tk;
+  String bedrag = String(b);
+  String transactieKosten = String(tk);
+  String totaalBedrag = String(tb);
+  printBon(datum, tijd, iban, pasnummer, transactieId, bedrag, transactieKosten, totaalBedrag);
 }
 
 void right(){
@@ -95,7 +58,69 @@ void large(){
   printer.setSize('L');
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void printBon(String datum, String tijd, String iban, String pasnummer, String transactieId, String bedrag, String transactieKosten, String totaalBedrag){
+  printer.feed(2);
 
+  left();
+  large();
+  printer.boldOn();
+  printer.println("SYMPLE");
+  printer.boldOff();
+
+  center();
+  printLine();
+
+  medium();
+  center();
+  printer.setLineHeight(10);
+  printer.println("OPNAME BILJETTEN");
+  printer.setLineHeight();
+
+  printer.feed(1);
+
+  left();
+  small();
+  printer.println("DATUM\t\tTIJD\t\tAUTOMAAT");
+  printer.print(datum);
+  printer.print("\t");
+  printer.print(tijd);
+  printer.println("\t\t1");
+
+  printer.feed(1);
+
+  //Rekeningnummer ten alle tijden deels afgeschermd houden
+  printer.println("REKENINGNUMMER\t\t\tPASNR");
+  printer.print("NLxx SMPL xxxx xx");
+  printer.print(iban);
+  printer.print("\t");
+  printer.println(pasnummer);
+
+  printer.feed(1);
+
+  printer.print("TRANSACTIE ID\t\t:\t");
+  printer.println(transactieId);
+
+  printer.feed(1);
+
+  printer.print("BEDRAG\t\t\t\t:\t");
+  printer.println(bedrag);
+  printer.print("TRANSACTIE KOSTEN\t:\t");
+  printer.println(transactieKosten);
+
+  printer.feed(1);
+
+  printer.print("TOTAAL BEDRAG\t\t:\t");
+  printer.println(totaalBedrag);
+
+  printer.feed(2);
+
+  medium();
+  center();
+  printer.println("TOT ZIENS");
+
+  printer.feed(4);
+}
+
+void loop() {
+  
 }
