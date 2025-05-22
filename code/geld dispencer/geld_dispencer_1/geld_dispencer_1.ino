ed40a6fa0b193523f4d754716864d506c26517ea
BruddaCode <99719149+BruddaCode@users.noreply.github.com>
2025-05-22 12:14:27 +0200

    Update geld_dispencer_1.ino

--- code/geld dispencer/geld_dispencer_1/geld_dispencer_1.ino
+++ code/geld dispencer/geld_dispencer_1/geld_dispencer_1.ino
@@ -1,24 +1,90 @@
-#define mp1 8
-#define mp2 7
+#define disp1_1 2
+#define disp1_2 3
+#define disp2_1 4
+#define disp2_2 5 
+#define disp3_1 6
+#define disp3_2 7
 
 void setup() {
-  pinMode(mp1, OUTPUT);
-  pinMode(mp2, OUTPUT);
+  pinMode(disp1_1, OUTPUT);
+  pinMode(disp1_2, OUTPUT);
+  pinMode(disp2_1, OUTPUT);
+  pinMode(disp2_2, OUTPUT);
+  pinMode(disp3_1, OUTPUT);
+  pinMode(disp3_2, OUTPUT);
+
+  stop(4);  
 }
+
 
 void loop() {
-  push();
-  delay(2000);
-  pull();
+  push(1);
+  push(2);
+  push(3);
+  delay(3000);
+  pull(1);
+  pull(2);
+  pull(3);
   delay(1000);
+  stop(4);
+  delay(2000);
 }
 
-void push(){
-  digitalWrite(mp1, HIGH);
-  digitalWrite(mp2, LOW);
+void push(int dispenser){
+  switch(dispenser) {
+    case 1:
+      digitalWrite(disp1_1, LOW);
+      digitalWrite(disp1_2, HIGH);
+      break;
+    case 2: 
+      digitalWrite(disp2_1, LOW);
+      digitalWrite(disp2_2, HIGH);
+      break;
+    case 3:
+      digitalWrite(disp3_1, LOW);
+      digitalWrite(disp3_2, HIGH);
+      break;
+  }
 }
 
-void pull(){
-  digitalWrite(mp1, LOW);
-  digitalWrite(mp2, HIGH);
+void pull(int dispenser){
+  switch(dispenser) {
+    case 1:
+      digitalWrite(disp1_1, HIGH);
+      digitalWrite(disp1_2, LOW);
+      break;
+    case 2: 
+      digitalWrite(disp2_1, HIGH);
+      digitalWrite(disp2_2, LOW);
+      break;
+    case 3:
+      digitalWrite(disp3_1, HIGH);
+      digitalWrite(disp3_2, LOW);
+      break;
+  }
+}
+
+void stop(int dispenser){
+  switch(dispenser) {
+    case 1:
+      digitalWrite(disp1_1, LOW);
+      digitalWrite(disp1_2, LOW);
+      break;
+    case 2: 
+      digitalWrite(disp2_1, LOW);
+      digitalWrite(disp2_2, LOW);
+      break;
+    case 3:
+      digitalWrite(disp3_1, LOW);
+      digitalWrite(disp3_2, LOW);
+      break;
+    case 4:
+      digitalWrite(disp1_1, LOW);
+      digitalWrite(disp1_2, LOW);
+      digitalWrite(disp2_1, LOW);
+      digitalWrite(disp2_2, LOW);
+      digitalWrite(disp3_1, LOW);
+      digitalWrite(disp3_2, LOW);
+      break;
+  }
 }
